34bad14a47effa3ecea50b46b1302ce16be20f38
fsx <a7210316@qq.com>
2013-03-23 18:40:58 +0800

    初步更改

--- BuildDream/settings.py
+++ BuildDream/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'BuildDream',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'shixuemeiily    ',
+        'PASSWORD': 'shixuemeiily',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'User'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
